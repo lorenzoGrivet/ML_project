4fcb1b95a392f00709a4699899b64e30f8a49988
andreaGaudino <andrea.gaudino02@gmail.com>
2025-07-08 17:43:27 +0200

    PPO initial commit

---
+++ test_sb3.py
@@ -0,0 +1,93 @@
+"""Test an RL agent trained with Stable-Baselines3 on the CustomHopper environment"""
+import argparse
+import os
+import time
+
+import gym
+import wandb
+import numpy as np
+from stable_baselines3 import PPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from env.custom_hopper import *
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--episodes', type=int, default=200, help='Number of test episodes')
+    parser.add_argument('--render', default=False, action='store_true', help='Render the simulator')
+    parser.add_argument('--device', type=str, default='cpu', help='Device to run the model on [cpu, cuda]')
+    parser.add_argument('--name', type=str, default='sb3-test', help='Name of the test run')
+    parser.add_argument("--mod_test", default="source",type=str,help='Test mode' )
+    parser.add_argument("--mod_train", required=True,type=str, help='Train mode')
+
+
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    mod_test=args.mod_test
+
+    # Output file
+    os.makedirs(args.name, exist_ok=True)
+    out_file_path = os.path.join(args.name, f"output_train_{args.mod_train}_test_{mod_test}.txt")
+    out_file = open(out_file_path, "w")
+
+    env = gym.make(f'CustomHopper-{mod_test}-v0')
+
+    print("Action space:", env.action_space)
+    print("State space:", env.observation_space)
+    print("Dynamics parameters:", env.get_parameters())
+    
+    
+    out_file.write(f"Model tested with {mod_test}\n")
+
+    out_file.write(f"Action space: {env.action_space}\n")
+    out_file.write(f"State space: {env.observation_space}\n")
+    out_file.write(f"Dynamics parameters: {env.get_parameters()}\n")
+
+
+    wandb.init(
+        project="PPO",
+        name=f"{args.name}_train_{args.mod_train}_test_{mod_test}",
+        entity="andrea-gaudino02-politecnico-di-torino",
+        config={
+            "episodes": args.episodes,
+            "render": args.render,
+            "device": args.device,
+            "env": "CustomHopper-target-v0"
+        }
+    )
+
+    model = PPO.load(f"{args.name}/best_model_train_{args.mod_train}/best_model.zip", device=args.device)
+
+
+    for episode in range(1, args.episodes + 1):
+        done = False
+        state = env.reset()
+        test_reward = 0
+        start_time = time.time()
+
+        while not done:
+            action, _ = model.predict(state, deterministic=True)
+            state, reward, done, _ = env.step(action)
+
+            if args.render:
+                env.render()
+
+            test_reward += reward
+
+        duration = time.time() - start_time
+
+        print(f"Episode {episode} | Return: {test_reward:.2f} | Duration: {duration:.2f}s")
+        out_file.write(f"Episode {episode} | Return: {test_reward:.2f} | Duration: {duration:.2f}s\n")
+        wandb.log({
+            "episode": episode,
+            "test_reward": test_reward,
+            "test_duration": duration
+        }, step=episode)
+
+
+    out_file.close()
+    wandb.finish()
+
+if __name__ == '__main__':
+    main()
--- train_sb3.py
+++ train_sb3.py
@@ -4,19 +4,125 @@
     Read the stable-baselines3 documentation and implement a training
     pipeline with an RL algorithm of your choice between PPO and SAC.
 """
+import os
 import gym
+import argparse
 from env.custom_hopper import *
+import wandb
+from wandb.integration.sb3 import WandbCallback
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback, CallbackList
+from stable_baselines3.common.evaluation import evaluate_policy
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--episodes', default=1_000_000, type=int, help='Number of training episodes')
+    parser.add_argument('--render', default=False, type=bool)
+    parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
+    parser.add_argument('--name', default='hopper-train_noName', type=str, help='Scegliere nome')
+    parser.add_argument("--mod_train", default="source",type=str)
+
+
+    
+    return parser.parse_args()
+
+args = parse_args()
+
+mod_train=args.mod_train
+mod_eval=mod_train
 
 def main():
-    train_env = gym.make('CustomHopper-source-v0')
+   
+    wandb.init(
+        project="PPO",
+        name=f"{args.name}_train_{mod_train}",
+        entity="andrea-gaudino02-politecnico-di-torino",
+        config={
+            "env": "CustomHopper-source-v0",
+            "algorithm": "PPO",
+            "total_timesteps": args.episodes,
+            "eval_freq": 10_000,
+            "n_eval_episodes": 5
+        },
+        sync_tensorboard=True,
+        monitor_gym=True
+    )
+    config = wandb.config
+    
+
+    train_env = gym.make(f'CustomHopper-{mod_train}-v0')
+    eval_env = gym.make(f'CustomHopper-{mod_eval}-v0')
+
 
     print('State space:', train_env.observation_space)  # state-space
     print('Action space:', train_env.action_space)  # action-space
     print('Dynamics parameters:', train_env.get_parameters())  # masses of each link of the Hopper
+    
+    
+    os.makedirs(args.name, exist_ok=True)
+    out_file_name = f"{args.name}/output_train_{mod_train}.txt"
+    out_file = open(out_file_name, "w")
+    
+    
+    out_file.write(f"Action space: {train_env.action_space}\n")
+    out_file.write(f"State space: {train_env.observation_space}\n")
+    out_file.write(f"Dynamic parameters: {train_env.get_parameters()}\n")
+    
+    out_file.write(f"\nModel trainato con {mod_train}")
+    out_file.write(f"Model eval con {mod_eval}\n")
+        
+    
 
     #
     # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
     #
+
+
+    model = PPO(
+        "MlpPolicy",
+        train_env,
+        verbose=1,
+        tensorboard_log=f"{args.name}/runs/"
+    )
+
+
+    eval_callback = EvalCallback(
+        eval_env,
+        best_model_save_path=f"{args.name}/best_model_train_{mod_train}/",
+        log_path=f"{args.name}/eval_logs/",
+        eval_freq=config.eval_freq,
+        n_eval_episodes=config.n_eval_episodes,
+        deterministic=True, 
+        render=args.render
+        )
+
+    
+    wandb_callback = WandbCallback(
+        model_save_path=f"{args.name}/models/",
+        verbose=2
+    )
+
+    callback = CallbackList([wandb_callback, eval_callback])
+
+    model.learn(
+        total_timesteps=config.total_timesteps,
+        callback=callback
+    )
+
+
+    
+    mean_reward, std_reward = evaluate_policy(
+        model,
+        eval_env,
+        n_eval_episodes=config.n_eval_episodes,
+        deterministic=True
+    )
+    print(f"Final evaluation mean reward: {mean_reward:.2f} +/- {std_reward:.2f}")
+    
+    out_file.write(f"Final evaluation mean reward: {mean_reward:.2f} +/- {std_reward:.2f}")
+    out_file.close()
+
+    wandb.finish()
 
 if __name__ == '__main__':
     main()
